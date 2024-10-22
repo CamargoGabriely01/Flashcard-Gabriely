e602bcd59dafca28c9d6e1a5096097394957f19d
CamargoGabriely01 <168448630+CamargoGabriely01@users.noreply.github.com>
2024-10-22 09:12:21 -0300

    ok

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,36 @@
-  
+criaCartao(
+    "Programação",
+    "O que é JavaScript?",
+    "O JavaScript é uma linguagem de programação",
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta,
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta,
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta,
+)
+
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta,
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta,
+)
