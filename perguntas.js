929968fc0850b91e327036106e32f9c7179d16d1
CamargoGabriely01 <168448630+CamargoGabriely01@users.noreply.github.com>
2024-11-18 11:09:27 -0300

    ok

--- perguntas.js
+++ perguntas.js
@@ -11,9 +11,9 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'astronomia',
+    'qual o menor planeta do sistema solar?',
+    'mercúrio'
 )
 
 criaCartao(
